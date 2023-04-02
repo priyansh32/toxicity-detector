cb3cb6ae63b6a8c7bd603ccad42179483d7d4737
priyansh32 <patidar.priyansh936@gmail.com>
2023-04-03 01:51:18 +0530

    I finally figured out how to useEffect

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,9 +1,8 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import LoadingScreen from "./LoadingScreen";
 
 const toxicity = require("@tensorflow-models/toxicity");
-let model;
-toxicity.load(0.7).then((m) => (model = m));
 
 export default function Main() {
   const [result, setResult] = useState([
@@ -16,7 +15,13 @@ export default function Main() {
     { label: "Toxicity", probability: 0, match: false },
   ]);
   const [loading, setLoading] = useState(false);
-  //   const [model, _] = useState(modell);
+  const [modelLoaded, setmodelLoaded] = useState(false);
+  const [model, setModel] = useState(null);
+
+  useEffect(() => {
+    toxicity.load(0.7).then((m) => setModel(m));
+    setmodelLoaded(true);
+  }, []);
 
   function runModel(sentences) {
     let result_array = result;
@@ -35,89 +40,99 @@ export default function Main() {
   }
   return (
     <>
-      <div className='flex justify-center max-w-3xl mx-auto py-6 sm:px-6 lg:px-8'>
-        <div className='bg-white overflow-hidden shadow rounded-lg w-11/12 max-w-lg'>
-          <div className='px-4 py-5 sm:p-6'>
-            <h1 className='flex-shrink-0 py-4 font-bold'>
-              Toxicity Detector - TensorflowJS
-            </h1>
-            <div className='min-w-0 flex-1'>
-              <form action='#'>
-                <div className='border-b border-gray-300 focus-within:border-indigo-600'>
-                  <label htmlFor='comment' className='sr-only'>
-                    Text here...
-                  </label>
-                  <textarea
-                    rows={3}
-                    name='comment'
-                    id='comment'
-                    className='block w-full border-0 border-b border-transparent p-0 pb-2 resize-none focus:ring-0 focus:border-indigo-600 sm:text-sm'
-                    placeholder='Add your text...'
-                    defaultValue={""}
-                  />
-                </div>
-                <div className='pt-2 flex justify-end'>
-                  <div className='flex-shrink-0'>
-                    <button
-                      type='submit'
-                      className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setLoading(true);
-                        runModel([document.getElementById("comment").value]);
-                      }}
-                    >
-                      {loading ? (
-                        <>
-                          <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            strokeWidth='1.5'
-                            stroke='currentColor'
-                            className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
-                          >
-                            <path
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              d='M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99'
-                            />
-                          </svg>
-                          Calculating
-                        </>
-                      ) : (
-                        <>Check Toxicity</>
-                      )}
+      {!modelLoaded ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <div className='flex justify-center max-w-3xl mx-auto py-6 sm:px-6 lg:px-8'>
+            <div className='bg-white overflow-hidden shadow rounded-lg w-11/12 max-w-lg'>
+              <div className='px-4 py-5 sm:p-6'>
+                <h1 className='flex-shrink-0 py-4 font-bold'>
+                  Toxicity Detector - TensorflowJS
+                </h1>
+                <div className='min-w-0 flex-1'>
+                  <form action='#'>
+                    <div className='border-b border-gray-300 focus-within:border-indigo-600'>
+                      <label htmlFor='comment' className='sr-only'>
+                        Text here...
+                      </label>
+                      <textarea
+                        rows={3}
+                        name='comment'
+                        id='comment'
+                        className='block w-full border-0 border-b border-transparent p-0 pb-2 resize-none focus:ring-0 focus:border-indigo-600 sm:text-sm'
+                        placeholder='Add your text...'
+                        defaultValue={""}
+                      />
+                    </div>
+                    <div className='pt-2 flex justify-end'>
+                      <div className='flex-shrink-0'>
+                        <button
+                          type='submit'
+                          className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setLoading(true);
+                            runModel([
+                              document.getElementById("comment").value,
+                            ]);
+                          }}
+                        >
+                          {loading ? (
+                            <>
+                              <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                                strokeWidth='1.5'
+                                stroke='currentColor'
+                                className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
+                              >
+                                <path
+                                  strokeLinecap='round'
+                                  strokeLinejoin='round'
+                                  d='M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99'
+                                />
+                              </svg>
+                              Calculating
+                            </>
+                          ) : (
+                            <>Check Toxicity</>
+                          )}
 
-                      {/* Check Toxicity */}
-                    </button>
-                  </div>
+                          {/* Check Toxicity */}
+                        </button>
+                      </div>
+                    </div>
+                  </form>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className='flex flex-col justify-center max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
-        <h3 className='text-lg leading-6 font-medium text-gray-900'>Result</h3>
-        <dl className='mt-5 grid grid-cols-2 gap-5 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7'>
-          {result.map((item) => (
-            <div
-              key={item.label}
-              className={`px-4 py-5 shadow rounded-lg overflow-hidden sm:p-6 ${
-                item.match ? "bg-red-400" : "bg-green-400"
-              }`}
-            >
-              <dt className='text-sm font-medium text-gray-900 truncate'>
-                {item.label}
-              </dt>
-              <dd className='mt-1 text-2xl font-medium text-gray-900'>
-                {`${item.probability}%`}
-              </dd>
-            </div>
-          ))}
-        </dl>
-      </div>
+          <div className='flex flex-col justify-center max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
+            <h3 className='text-lg leading-6 font-medium text-gray-900'>
+              Result
+            </h3>
+            <dl className='mt-5 grid grid-cols-2 gap-5 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7'>
+              {result.map((item) => (
+                <div
+                  key={item.label}
+                  className={`px-4 py-5 shadow rounded-lg overflow-hidden sm:p-6 ${
+                    item.match ? "bg-red-400" : "bg-green-400"
+                  }`}
+                >
+                  <dt className='text-sm font-medium text-gray-900 truncate'>
+                    {item.label}
+                  </dt>
+                  <dd className='mt-1 text-2xl font-medium text-gray-900'>
+                    {`${item.probability}%`}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </>
+      )}
     </>
   );
 }
