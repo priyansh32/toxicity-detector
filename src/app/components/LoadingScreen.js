cf4d80760d5df7211807129892534965e3c788c7
priyansh32 <patidar.priyansh936@gmail.com>
2023-04-03 13:04:58 +0530

    more cool loader

--- src/app/components/LoadingScreen.js
+++ src/app/components/LoadingScreen.js
@@ -1,12 +1,22 @@
+import Image from "next/image";
+
 export default function LoadingScreen() {
   return (
-    <div className='flex h-screen items-center justify-center max-w-7xl mx-auto sm:px-6 lg:px-8'>
+    <div className='flex flex-col h-screen items-center justify-center max-w-7xl mx-auto sm:px-6 lg:px-8'>
+      {/* <div */}
+      <Image
+        alt='a cat sitting and waiting'
+        src={"/09c74fd8-9d51-4987-b8b5-a964bff73b3d.jpg"}
+        width={100}
+        height={100}
+      />
       <svg
         aria-hidden='true'
-        class='w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-100 fill-blue-600'
+        className='w-6 h-6 relative text-gray-100 animate-spin dark:text-gray-100 fill-blue-600'
         viewBox='0 0 100 101'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
+        style={{ top: "-62px" }}
       >
         <path
           d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
@@ -17,7 +27,7 @@ export default function LoadingScreen() {
           fill='currentFill'
         />
       </svg>
-      <span class='sr-only'>Loading...</span>
+      <span className='sr-only'>Loading...</span>
     </div>
   );
 }
